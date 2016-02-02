abc9d95a8d396e161b434fa2d5b4921e999d30b8
HzJavier <javier.hernandez@wizeservices.com>
2016-02-01 22:45:48 -0600

    Implement basic structure of challenge 1

---
+++ public/components/metro-music.js
@@ -0,0 +1,29 @@
+var MetroMusicLogin = React.createClass({
+  render: function () {
+    return (
+      <div className="metro-music-login">
+        <div className="title">
+          Metro Music
+        </div>
+
+        <button className="facebook-btn">SIGN UP WITH FACEBOOK</button>
+        <span>OR</span>
+        <button className="create-btn">CREATE ACCOUNT</button>
+
+        <form>
+          <input type="text" name="username" placeholder="Username"></input>
+          <input type="text" name="email" placeholder="E-mail"></input>
+          <input type="password" name="password" placeholder="Password"></input>
+          <button className="sign-up-btn">SIGN UP</button>
+        </form>
+
+        <div className="disclaimer">
+          To Sign Up you must agree to our Terms of Service and Privacy Policy
+        </div>
+
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<MetroMusicLogin/>, document.getElementById('lol'));
